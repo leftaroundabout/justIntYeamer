d0f29d8c191e88e323234538596fc08ddd5c8962
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-09-08 19:43:54 +0200

    Slightly more exciting sound.

--- Sound/Tone/Simple.hs
+++ Sound/Tone/Simple.hs
@@ -15,21 +15,32 @@ import qualified Sound.File.Sndfile.Buffer.StorableVector as HSnd
 import qualified Data.StorableVector as UArr
 
 import Data.Int
+import Control.Arrow
 
 type Frequency = Double  -- In Hertz
 type Duration = Double   -- In seconds
+type Amplitude = Double
 
 νSample :: Frequency
 νSample = 44100
 
+overdrive :: Amplitude -> Amplitude
+overdrive p = p / (1 + p^2)
+
 makeTone :: Duration -> Frequency -> FilePath -> IO ()
 makeTone t ν tgt = fmap mempty . HSnd.writeFile info tgt
           . HSnd.toBuffer . UArr.sample nSpl
-         $ \i -> let φ = ω'Spl*fromIntegral i
-                 in round $ fromIntegral (maxBound :: Int16) * sin φ :: Int16
+         $ fromIntegral >>> \i
+             -> let φ = ω'Spl*i
+                    t = tSpl*i
+                 in round $ fromIntegral (maxBound :: Int16)
+                            * overdrive (ampl * (sin φ + sin (2*φ)) * exp (-6*t))
+                      :: Int16
  where info = HSnd.Info nSpl (round νSample) 1 sndFormat 1 True
        nSpl = round $ t * νSample
        ω'Spl = 2*pi*ν/νSample
+       tSpl = 1/νSample
+       ampl = 10 / sqrt ν
 
 sndFormat :: HSnd.Format
 sndFormat = HSnd.Format {
