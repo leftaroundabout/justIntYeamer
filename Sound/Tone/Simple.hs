a1c61bc85cb3f0dfd3b0a5f42e4fe306a82a09a3
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-09-08 19:21:35 +0200

    Very simple tone generator.

--- Sound/Tone/Simple.hs
+++ Sound/Tone/Simple.hs
@@ -8,13 +8,32 @@
 -- Portability : portable
 -- 
 
-module Sound.Tone.Simple where
+module Sound.Tone.Simple (Frequency, Duration, makeTone) where
     
-import Sound.File.Sndfile.Buffer.StorableVector
-import qualified Data.Vector.Storable as UArr
+import qualified Sound.File.Sndfile as HSnd
+import qualified Sound.File.Sndfile.Buffer.StorableVector as HSnd
+import qualified Data.StorableVector as UArr
+
+import Data.Int
 
 type Frequency = Double  -- In Hertz
 type Duration = Double   -- In seconds
 
-makeTone :: Duration -> Frequency -> IO FilePath
-makeTone t ν = undefined
+νSample :: Frequency
+νSample = 44100
+
+makeTone :: Duration -> Frequency -> FilePath -> IO ()
+makeTone t ν tgt = fmap mempty . HSnd.writeFile info tgt
+          . HSnd.toBuffer . UArr.sample nSpl
+         $ \i -> let φ = ω'Spl*fromIntegral i
+                 in round $ fromIntegral (maxBound :: Int16) * sin φ :: Int16
+ where info = HSnd.Info nSpl (round νSample) 1 sndFormat 1 True
+       nSpl = round $ t * νSample
+       ω'Spl = 2*pi*ν/νSample
+
+sndFormat :: HSnd.Format
+sndFormat = HSnd.Format {
+    HSnd.headerFormat = HSnd.HeaderFormatWav
+  , HSnd.sampleFormat = HSnd.SampleFormatPcm16
+  , HSnd.endianFormat = HSnd.EndianFile
+  }
